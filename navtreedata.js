293c880f9b20197857de7edc1f029b7d1fe9b3a3
pshnghng0318 <128131821+pshnghng0318@users.noreply.github.com>
2026-03-30 06:06:19 +0000

    Deploying to gh-pages from @ pshnghng0318/carta-backend@9e9d4ed080c4b71be64ec233dbf3a09906388c74 🚀

--- navtreedata.js
+++ navtreedata.js
@@ -26,6 +26,7 @@ var NAVTREE =
 [
   [ "CARTA Backend", "index.html", [
     [ "Welcome to the CARTA backend documentation", "index.html", "index" ],
+    [ "Carta-casacore upgrade checklist", "md_docs_2static_2casacore.html", null ],
     [ "Developer FAQ and tips", "md_docs_2static_2faq.html", [
       [ "Building and running unit tests", "md_docs_2static_2faq.html#autotoc_md0", null ],
       [ "Building with ASAN flags", "md_docs_2static_2faq.html#autotoc_md1", null ],
@@ -81,25 +82,25 @@ var NAVTREEINDEX =
 [
 "AnimationObject_8h.html",
 "Image_8h.html#a9a542ab2ca3525dedc7a1f5c9ca4e637",
-"classMessage.html#a1dcda15dd079ae04209f6e8e28bd3980",
-"classcarta_1_1CartaFitsImage.html#adf1edb5f38dbdab23926337881b6aa8e",
-"classcarta_1_1CompressedFits.html#a3c6fd7a3a5321b32240beb9e1844950c",
-"classcarta_1_1Ds9Importer.html#a9db44517a54a7da3a2f878e152898e1d",
-"classcarta_1_1FileLoader.html#aab4ad60b38570c3e8e209756abec12a3",
-"classcarta_1_1Frame.html#a97b7d68a5bc87383438147fefcd77cbc",
-"classcarta_1_1HttpServer.html#a44362263707890daf7c642f9c8400877",
-"classcarta_1_1ImageMoments.html#a8c5effa3dc190a4a8ccb203233cde313",
-"classcarta_1_1PolarizationCalculator.html#a2cb6200df64e7dde3eacf173a97f894c",
-"classcarta_1_1RegionConverter.html#a3cb57550a686487734aa79c4e88d89e2",
-"classcarta_1_1RegionHandler.html#adf0a26d068e926212c682fdf15d367fe",
-"classcarta_1_1Session.html#a5fd62896cf8474c7b37c27093a24459e",
-"classcarta_1_1SessionManager.html#a3703920a32dfd9602379759b23dd69c5",
-"classcarta_1_1TableController.html#a0247e026291c116084057fab005453ed",
-"classcarta_1_1concurrent__queue.html",
-"namespacecarta.html#a95828b5aa32c7a2363d7679a2cb91b77",
-"structcarta_1_1ContourSettings.html#ab7c82f7bdcd9a01d1e121bcd5f6feedb",
-"structcarta_1_1ProgramSettings.html#a2521653ef67d47c6b48aa3d4a74edcfb",
-"structcarta_1_1StokesSource.html#a604900ea270d364968262d89b6e03378"
+"classMessage.html#a3e6ef484d8d2026c2d7c5381ac7ece1e",
+"classcarta_1_1CartaHdf5Image.html#a244202bc52a2c614595faf0363f63f8e",
+"classcarta_1_1CompressedFits.html#a94e623f08d835889943c9d7a2fcb7f76",
+"classcarta_1_1ExprLoader.html#a81ca6bd8251b0a7a6285ddd6a24a59e7",
+"classcarta_1_1FileLoader.html#ac8af33ecfa4dd712b78e91ec4221dcb3",
+"classcarta_1_1Frame.html#aab2a5d7c4ae3e484737b123b28274f3b",
+"classcarta_1_1HttpServer.html#a63128482ebdede9e4da3e24fd4bf0c7e",
+"classcarta_1_1ImagePtrLoader.html#a822d130b7bb626e46b446aa1cb5184ab",
+"classcarta_1_1PolarizationCalculator.html#ab48307d44c2a4541caa908d24a3683a0",
+"classcarta_1_1RegionConverter.html#a8e11218664543fd77748b2ea4eb2be94",
+"classcarta_1_1RegionHistogram.html#a24b3b5d75b44a683d4aecd553362d8d7",
+"classcarta_1_1Session.html#a6e3719e8d244e28cdf5a80725b0727ff",
+"classcarta_1_1SessionManager.html#a6069680ecd3c9b67b288d12c2a721b11",
+"classcarta_1_1TableController.html#a5bdc7306475a622a9957430692853b6e",
+"classcarta_1_1queuing__rw__mutex.html#a0c2331425fda539271288468819487a4",
+"namespacecarta.html#ad170096e224b353fb6640af99aa07cd2",
+"structcarta_1_1EventHeader.html#a2c7db1be4d006cc7e9513ef3cd48039f",
+"structcarta_1_1ProgramSettings.html#a3cf6f38321c0e0439b745db850cd94e8",
+"structcarta_1_1TableViewCache.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
